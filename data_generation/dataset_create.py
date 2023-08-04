3153b8d1b56193ac13783df31eb91748a7bb9551
Albert <albert.sola9@gmail.com>
2023-08-04 21:28:08 +0200

    Final results

--- data_generation/dataset_create.py
+++ data_generation/dataset_create.py
@@ -1,6 +1,4 @@
 import sys
-import numpy as np
-import pandas as pd
 from wntr_utils import *
 
 # get configurations
@@ -11,11 +9,13 @@ df_counts = get_wdn_components(networks, path)
 ordered_networks = df_counts.sort_values(by=['nodes']).index.tolist()
 
 for i, network in enumerate(ordered_networks):
-    out_path = os.getcwd()+'\\datasets\\'
+    out_path = os.getcwd() + '\\datasets\\'
     for ix_trials, trials in enumerate(n_trials):
-        print(f'Working with {network}, network {i+1} of {len(ordered_networks)} || size: {trials}, {ix_trials+1} of {len(n_trials)}')
+        print(
+            f'Working with {network}, network {i + 1} of {len(ordered_networks)} || size: {trials}, {ix_trials + 1} of {len(n_trials)}')
         try:
-            create_and_save(network, path, n_trials=trials, d_attr=d_attr, d_netw=d_netw, out_path=out_path, max_fails=10*trials, show=True)
+            create_and_save(network, path, n_trials=trials, d_attr=d_attr, d_netw=d_netw, out_path=out_path,
+                            max_fails=10 * trials, show=True)
         except:
             print("Too many failed simulations")
             break
--- data_generation/wntr_utils.py
+++ data_generation/wntr_utils.py
@@ -35,7 +35,7 @@ def run_wntr_simulation(wn, headloss='H-W'):
     wn.options.hydraulic.viscosity = 1.0
     wn.options.hydraulic.specific_gravity = 1.0
     wn.options.hydraulic.demand_multiplier = 1.0
-    wn.options.hydraulic.demand_model = 'PDD'
+    wn.options.hydraulic.demand_model = 'DD'
     wn.options.hydraulic.minimum_pressure = 0
     wn.options.hydraulic.required_pressure = 1
     wn.options.hydraulic.pressure_exponent = 0.5
@@ -49,6 +49,7 @@ def run_wntr_simulation(wn, headloss='H-W'):
     wn.options.hydraulic.damplimit = 0.0
     wn.options.hydraulic.headerror = 0.0
     wn.options.hydraulic.flowchange = 0.0
+    wn.options.hydraulic.inpfile_units = "LPS"
     sim = wntr.sim.EpanetSimulator(wn)
     results = sim.run_sim(version=2.2)
 
@@ -396,14 +397,14 @@ def from_wntr_to_nx(wn):
     uG_WDS = G_WDS.to_undirected()  # undirected
     sG_WDS = nx.Graph(uG_WDS)  # Simple graph
 
-    i = 0
-    for (u, v, wt) in sG_WDS.edges.data():
-        assert isinstance(wn_links[i][1], wntr.network.elements.Pipe), "The link is not a pipe"
-        sG_WDS[u][v]['name'] = wn_links[i][1].name
-        sG_WDS[u][v]['diameter'] = wn_links[i][1].diameter
-        sG_WDS[u][v]['length'] = wn_links[i][1].length
-        sG_WDS[u][v]['roughness'] = wn_links[i][1].roughness
-        i += 1
+    for (u,v,wt) in sG_WDS.edges.data():
+        for edge in wn.links():
+            if (edge[1].start_node.name == u and edge[1].end_node.name == v) or (edge[1].start_node.name == v and edge[1].end_node.name == u):
+                assert isinstance(edge[1], wntr.network.elements.Pipe), "The link is not a pipe"
+                sG_WDS[u][v]['name'] = edge[1].name
+                sG_WDS[u][v]['diameter'] = edge[1].diameter
+                sG_WDS[u][v]['length'] = edge[1].length
+                sG_WDS[u][v]['roughness'] = edge[1].roughness
 
     i = 0
     for u in sG_WDS.nodes:
