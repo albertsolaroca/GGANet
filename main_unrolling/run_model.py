334922d26893c1156f35c42f2509cb2d6097c90f
nmertzanis <nmertzanis@gmail.com>
2024-02-08 13:32:41 +0100

    Update run_model.py

--- main_unrolling/run_model.py
+++ main_unrolling/run_model.py
@@ -2,7 +2,7 @@ import torch
 from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
-from main_unrolling.training.models import Dummy
+from training.models import Dummy
 from training.test import testing_plain
 from utils.load import load_raw_dataset
 from tune_train import prepare_training, default_configuration
@@ -29,32 +29,39 @@ def metrics(real, pred, dummy):
 
     dummy_score = r2_score(real, dummy, multioutput='variance_weighted')
     model_score = r2_score(real, pred, multioutput='variance_weighted')
-    print("R2-values \n", "Dummy:", dummy_score, "\n Model", model_score)
+    # print("R2-values \n", "Dummy:", dummy_score, "\n Model", model_score)
 
     dummy_score = mean_absolute_error(real, dummy)
     model_score = mean_absolute_error(real, pred)
-    print("MAE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
+    # print("MAE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
 
     dummy_score = mean_squared_error(real, dummy)
     model_score = mean_squared_error(real, pred)
-    print("MSE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
+    # print("MSE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
 
     dummy_score = mean_squared_error(real, dummy, squared=False)
     model_score = mean_squared_error(real, pred, squared=False)
-    print("RMSE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
+    # print("RMSE-values \n", "Dummy:", dummy_score, "\n Model", model_score)
 
     dummy_score = nse(real, dummy)
     model_score = nse(real, pred)
-    print("NSE-values General\n", "Dummy:", dummy_score, "\n Model", model_score)
+    # print("NSE-values General\n", "Dummy:", dummy_score, "\n Model", model_score)
 
     dummy_scores = []
     model_scores = []
-    for i in range(36):
+    for i in range(38):
         dummy_score = nse(real[:, i], dummy[:, i])
         model_score = nse(real[:, i], pred[:, i])
         dummy_scores.append(dummy_score)
         model_scores.append(model_score)
         print("NSE-values for node", i, "\n", "Dummy:", dummy_score, "\n Model", model_score)
+
+    # plt.plot(dummy_scores, label="Dummy")
+    plt.title('Model (UM)')
+    plt.plot(model_scores, label="Model", c="darkorange")
+    plt.ylabel('NSE')
+    plt.xlabel('Node')
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -69,7 +76,7 @@ if __name__ == "__main__":
     # retrieve wntr data
     tra_database, val_database, tst_database = load_raw_dataset(default_config.network, data_folder)
 
-    model_path = 'experiments/unrolling_WDN0027/FOS_pump_2/UnrollingModel/model.pickle'
+    model_path = '../main_unrolling/experiments/unrolling_WDN0350/FOS_pump_2/UnrollingModel/model.pickle'
     with open(model_path, 'rb') as handle:
         model = torch.load(handle)
         model.eval()
@@ -103,29 +110,29 @@ if __name__ == "__main__":
     # Array below is created to ensure proper indexing of the nodes when displaying
     type_array = (tst_database[0].node_type == 0) | (tst_database[0].node_type == 2)
 
-    for i in [0, 1, 6, 26, 36]:
-        plt.plot(real[0:100, i], label="Real")
-        plt.plot(pred[0:100, i], label="Predicted")
-        plt.plot(dummy[0:100, i], label="Dummy")
-        plt.ylabel('Head')
-        plt.xlabel('Timestep')
+    # for i in [0, 1, 6, 26, 36]:
+    #     plt.plot(real[0:100, i], label="Real")
+    #     plt.plot(pred[0:100, i], label="Predicted")
+    #     plt.plot(dummy[0:100, i], label="Dummy")
+    #     plt.ylabel('Head')
+    #     plt.xlabel('Timestep')
+    #
+    #     plt.legend()
+    #     names = {0: 'Next to Reservoir', 1: 'Random Node', 6: 'Next to Tank', 26: 'Random Node', 36: 'Tank', 37: 'Pump'}
+    #     plt.title(names[i])
+    #     # save_response_graphs_in_ML_tracker(real, pred, names[i], i)
+    #     plt.show()
+    #     plt.close()
 
-        plt.legend()
-        names = {0: 'Next to Reservoir', 1: 'Random Node', 6: 'Next to Tank', 26: 'Random Node', 36: 'Tank', 37: 'Pump'}
-        plt.title(names[i])
-        # save_response_graphs_in_ML_tracker(real, pred, names[i], i)
-        plt.show()
-        plt.close()
-
-    plt.plot(real[0:100, 37], label="Real")
-    plt.plot(pred[0:100, 37], label="Predicted")
-    plt.plot(dummy[0:100, 37], label="Dummy")
-    plt.ylabel('LPS')
-    plt.xlabel('Timestep')
-
-    plt.legend()
-    plt.title(names[37])
-    plt.show()
-    plt.close()
+    # plt.plot(real[0:100, 37], label="Real")
+    # plt.plot(pred[0:100, 37], label="Predicted")
+    # plt.plot(dummy[0:100, 37], label="Dummy")
+    # plt.ylabel('LPS')
+    # plt.xlabel('Timestep')
+    #
+    # plt.legend()
+    # plt.title(names[37])
+    # plt.show()
+    # plt.close()
 
     metrics(real, pred, dummy)
