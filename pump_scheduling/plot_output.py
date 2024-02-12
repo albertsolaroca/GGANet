21043edc51ee25fd2b5b95ab118cba1d36fde86d
nmertzanis <nmertzanis@gmail.com>
2024-02-12 10:25:22 +0100

    Plots refixed

--- pump_scheduling/plot_output.py
+++ pump_scheduling/plot_output.py
@@ -10,14 +10,16 @@ if __name__ == '__main__':
 
     score_mm = []
     score = []
-    avg_score_mm = []
-    avg_score = []
+    avg_energy_mm = []
+    avg_energy = []
+    avg_cost_mm = []
+    avg_cost = []
     cost = []
     energy = []
     cost_mm = []
     energy_mm = []
 
-    gen_range = range(4, 12, 1)
+    gen_range = range(5, 20, 1)
 
     for i in gen_range:
         i_gen_mm = ps_output_mm[ps_output_mm['n_generations'] == i]
@@ -31,10 +33,12 @@ if __name__ == '__main__':
         total_cost = i_gen['Cost (€) WNTR']
         total_energy = i_gen['Energy (kWh) WNTR']
 
-        score_mm.append(total_score_mm.values)
-        avg_score_mm.append(np.average(total_score_mm.values))
-        score.append(total_score.values)
-        avg_score.append(np.average(total_score.values))
+        avg_energy_mm.append(np.average(total_energy_mm.values))
+        avg_energy.append(np.average(total_energy.values))
+
+        avg_cost_mm.append(np.average(total_cost_mm))
+        avg_cost.append(np.average(total_cost))
+
         cost += list(total_cost.values)
         energy += list(total_energy.values)
         cost_mm += list(total_cost_mm.values)
@@ -57,24 +61,35 @@ if __name__ == '__main__':
     # plt.show()
     #
 
+    plt.plot(gen_range, avg_energy, '-o', label='EPANET - 34\'')
+    plt.plot(gen_range, avg_energy_mm, '-o',  label='Metamodel - 2\'')
+    plt.xlabel('Generations', fontsize=14)
+    plt.ylabel('kWh', fontsize=14)
+    plt.yticks(fontsize=12)
+    plt.xticks(fontsize=12)
+    plt.title('Energy consumption of optimal schedule', fontsize=15)
+    plt.legend(fontsize=14)
+    plt.show()
 
-    # plt.plot(gen_range, energy, label='EPANET - 34\'')
-    # plt.plot(gen_range, energy_mm, label='Metamodel - 2\'')
-    # plt.xlabel('Generations', fontsize=12)
-    # plt.ylabel('kWh', fontsize=12)
-    # plt.title('Energy consumption of optimal schedule', fontsize=13)
-    # plt.legend()
-    # plt.show()
+    plt.plot(gen_range, avg_cost, '-o', label='EPANET - 34\'')
+    plt.plot(gen_range, avg_cost_mm, '-o', label='Metamodel - 2\'')
+    plt.xlabel('Generations', fontsize=14)
+    plt.ylabel('€', fontsize=14)
+    plt.yticks(fontsize=12)
+    plt.xticks(fontsize=12)
+    plt.title('Cost of optimal schedule', fontsize=15)
+    plt.legend(fontsize=14)
+    plt.show()
 
     # plt.plot(ps_output_mm['n_generations'], ps_output_mm['Energy (kWh) WNTR'] + ps_output_mm['Cost (€) WNTR'])
     # plt.plot(ps_output['n_generations'], ps_output['Energy (kWh) WNTR'], label='WNTR')
     # plt.legend()
     # plt.show()
 
-    plt.scatter(energy, cost, label='EPANET - 34\'')
-    plt.scatter(energy_mm, cost_mm, label='Metamodel - 2\'')
-    plt.xlabel('Energy (kWh)', fontsize=12)
-    plt.ylabel('Cost (€)', fontsize=12)
-    plt.title('All solutions found', fontsize=13)
-    plt.legend()
-    plt.show()
+    # plt.scatter(energy, cost, label='EPANET - 34\'')
+    # plt.scatter(energy_mm, cost_mm, label='Metamodel - 2\'')
+    # plt.xlabel('Energy (kWh)', fontsize=12)
+    # plt.ylabel('Cost (€)', fontsize=12)
+    # plt.title('All solutions found', fontsize=13)
+    # plt.legend()
+    # plt.show()
